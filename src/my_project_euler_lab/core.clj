9ae5344d1a0fcb1a5792e9337633e3209f6c7d2d
Antoine Romain Dumont <antoine.romain.dumont@gmail.com>
2011-09-21 23:21:23 +0200

    Init the repository.

---
+++ src/my_project_euler_lab/core.clj
@@ -0,0 +1,6 @@
+(ns my-project-euler-lab.core
+  (:use [clojure.test               :only [run-tests]])
+  (:use [midje.sweet])
+  (:use [clojure.contrib.repl-utils :only [show]])
+  (:use [clojure.pprint             :only [pprint]])
+  (:use [clojure.walk               :only [macroexpand-all]]))
---
+++ test/my_project_euler_lab/test/core.clj
@@ -0,0 +1,6 @@
+(ns my-project-euler-lab.test.core
+  (:use [my-project-euler-lab.core])
+  (:use [clojure.test]))
+
+(deftest replace-me ;; FIXME: write
+  (is false "No tests have been written."))
