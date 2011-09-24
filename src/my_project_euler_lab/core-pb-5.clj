9af5d4f4be07f3c7f665630dea1df693aadebf12
Antoine Romain Dumont <antoine.romain.dumont@gmail.com>
2011-09-24 18:30:57 +0200

    Prepare the problem 5 file. 2520 is the smallest number that can be divided by each of the numbers from 1 to 10 without any remainder.
    What is the smallest positive number that is evenly divisible by all of the numbers from 1 to 20?

---
+++ src/my_project_euler_lab/core-pb-5.clj
@@ -0,0 +1,14 @@
+(ns my-project-euler-lab.core-pb-5
+  (:use [clojure.test               :only [run-tests]])
+  (:use [midje.sweet])
+  (:use [clojure.contrib.repl-utils :only [show]])
+  (:use [clojure.pprint             :only [pprint]])
+  (:use [clojure.walk               :only [macroexpand-all]]))
+
+                                        ; problem 5
+
+;2520 is the smallest number that can be divided by each of the numbers from 1 to 10 without any remainder.
+
+;What is the smallest positive number that is evenly divisible by all
+;of the numbers from 1 to 20?
+
