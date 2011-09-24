6e2926e17171d999b3ec9a34688432a5e44e0d99
Antoine Romain Dumont <antoine.romain.dumont@gmail.com>
2011-09-24 16:28:10 +0200

    Create the method to compute if a vector is a palindrome.

--- src/my_project_euler_lab/core-pb-4.clj
+++ src/my_project_euler_lab/core-pb-4.clj
@@ -12,3 +12,25 @@
 ;Find the largest palindrome made from the product of two 3-digit
 ;numbers.
 
+;((fact palimdromic-num 2) => [91 99])
+
+(defn pal? "Is the string given a palindrome?"
+  [v]
+  (loop [seq v]
+    (or (<= (count seq) 1)
+        (and (= (first seq) (last seq)) (recur (butlast (rest seq)))) 
+        )
+    )
+  )
+
+;.;. Work is either fun or drudgery. It depends on your attitude. I like
+;.;. fun. -- Barrett
+(fact
+  (pal? [1 2 3]) => false
+  (pal? [1 2 3 4]) => false
+  (pal? [1 2 2 1]) => true
+  (pal? ["1" "2" "2" "1"]) => true
+  (pal? ["1" "2" "2" "1" "2" "1"]) => false
+  (pal? ["1" "2" "2" "2" "2" "1"]) => true
+  (pal? [1 2 3 2 1]) => true
+  )
