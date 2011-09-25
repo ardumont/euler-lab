82c4ffd3e62033eb8021f3147992edc957201bc6
Antoine Romain Dumont <antoine.romain.dumont@gmail.com>
2011-09-25 10:54:57 +0200

    Sum of all prime numbers below 2 000 000 ? 142 913 828 922.

--- src/my_project_euler_lab/core-pb-10.clj
+++ src/my_project_euler_lab/core-pb-10.clj
@@ -4,7 +4,9 @@
   (:use [clojure.contrib.repl-utils :only [show]])
   (:use [clojure.pprint             :only [pprint]])
   (:use [clojure.walk               :only [macroexpand-all]])
-  (:use clojure.contrib.math))
+  (:use clojure.contrib.math)
+  ;(:use my-project-euler-lab.core-pb-7)
+  )
                                         ; problem 10
 
 ;The sum of the primes below 10 is 2 + 3 + 5 + 7 = 17.
@@ -22,7 +24,7 @@
   (if (zero? borne)
     0
     (loop [candidate 3 sum 2 primes [2]]
-    (if (<= borne candidate)
+    (if (< borne candidate)
       sum
       (if (every? #(not= 0 (rem candidate %)) (take (floor (sqrt candidate)) primes))
         (recur (+ 2 candidate) (+ sum candidate) (conj primes candidate))
@@ -31,7 +33,15 @@
     )
   )
 
+;.;. Work joyfully and peacefully, knowing that right thoughts and right
+;.;. efforts will inevitably bring about right results. -- Allen
 (fact
   (sum-prime-numbers 0) => 0
+  (sum-prime-numbers 1) => 2
+  (sum-prime-numbers 2) => 2
+  (sum-prime-numbers 3) => 5
+  (sum-prime-numbers 4) => 5
+  (sum-prime-numbers 5) => 10
+  (sum-prime-numbers 7) => 17
 )
 
