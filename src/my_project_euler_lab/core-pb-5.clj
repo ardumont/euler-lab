d977e4e94c2ccb500cb3bf69dc5af548906f27b5
Antoine Romain Dumont <antoine.romain.dumont@gmail.com>
2011-09-25 09:54:13 +0200

    Add the math contrib lib dependency.

--- src/my_project_euler_lab/core-pb-5.clj
+++ src/my_project_euler_lab/core-pb-5.clj
@@ -3,7 +3,8 @@
   (:use [midje.sweet])
   (:use [clojure.contrib.repl-utils :only [show]])
   (:use [clojure.pprint             :only [pprint]])
-  (:use [clojure.walk               :only [macroexpand-all]]))
+  (:use [clojure.walk               :only [macroexpand-all]])
+  (:use clojure.contrib.math))
 
                                         ; problem 5
 
@@ -12,6 +13,8 @@
 
 ;What is the smallest positive number that is evenly divisible by all
 ;of the numbers from 1 to 20?
+
+; brute force
 
 (defn euler-5-ok? "Can the number be divided by each of the numbers from 1 to borne without any remainder?"
   [num borne]
@@ -31,5 +34,6 @@
 ;.;. a rightly timed pause. -- Twain
 (fact(find-smallest-ok-euler-5 2 10 10) => 2520)
 ;(fact (find-smallest-ok-euler-5 2 20 20) => 232792560)
+
 
 
