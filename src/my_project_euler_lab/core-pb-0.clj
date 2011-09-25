c767aea2335eec7ba8031f1d2c23b5ca307cb297
Antoine Romain Dumont <antoine.romain.dumont@gmail.com>
2011-09-25 14:57:21 +0200

    Add other possible and more subtle theory implementations.

--- src/my_project_euler_lab/core-pb-0.clj
+++ src/my_project_euler_lab/core-pb-0.clj
@@ -20,3 +20,21 @@
 (fact (nn-mult-3-5 10) => [3 5 6 9])
 (fact (reduce + (nn-mult-3-5 10)) => 23)
 (fact (reduce + (nn-mult-3-5 1000)) => 233168)
+
+; more subtle
+; compute the sum of element multiple by 3 and not by 5 or by 5 and
+; not by 3 (at the same time)
+
+; even more subtle
+; sum 1 -> n = (* 1/2 n (inc n))
+
+;Let’s look at the details of our function and take as example n=3.
+;We would have to add:
+;3+6+9+12+......+999=3*(1+2+3+4+...+333)
+;For n=5 we would get:
+;5+10+15+...+995=5*(1+2+....+199)
+;Now note that 199=995/5 but also 999/5 rounded down to the nearest integer.
+;In many programming languages there exists a separate operator for
+;that: div or \.
+
+
