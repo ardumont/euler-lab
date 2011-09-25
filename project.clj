13f95511b4c9ed4daba3ac929822f989472729de
Antoine Romain Dumont <antoine.romain.dumont@gmail.com>
2011-09-25 09:53:06 +0200

    Update the clojure version to 1.3.0.

--- project.clj
+++ project.clj
@@ -1,5 +1,5 @@
 (defproject my-project-euler-lab "1.0.0-SNAPSHOT"
   :description "Solve euler problems"
-  :dependencies [[org.clojure/clojure "1.2.1"]]
+  :dependencies [[org.clojure/clojure "1.3.0"]]
   :dev-dependencies [[org.clojure/clojure-contrib "1.2.0"]
                      [midje "1.2.0"]] )
