dea86793dd1b6a57b573f07802f870a34f5d629c
Antoine Romain Dumont <antoine.romain.dumont@gmail.com>
2011-09-25 10:01:10 +0200

    Add the delimiter problem 7.

--- src/my_project_euler_lab/core-pb-7-10.clj
+++ src/my_project_euler_lab/core-pb-7-10.clj
@@ -6,6 +6,7 @@
   (:use [clojure.walk               :only [macroexpand-all]])
   (:use clojure.contrib.math))
 
+; Problem 7
 
 ; By listing the first six prime numbers: 2, 3, 5, 7, 11, and 13, 
 ; we can see that the 6th prime is 13.
