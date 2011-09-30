d699cb678406052dc1edf7f4a9be3dece6bb72bb
Antoine Romain Dumont <antoine.romain.dumont@gmail.com>
2011-09-30 07:11:53 +0200

    Just a function that i created by error. Compute the decomposition in prime number of a number. Must need improvment as it is bases on my prime-numbers-improved method (does not use the sieve of erathosthene).

---
+++ src/my_project_euler_lab/play_with_primes.clj
@@ -0,0 +1,54 @@
+(ns my-project-euler-lab.play_with_primes
+  (:use [clojure.test               :only [run-tests]])
+  (:use [midje.sweet])
+  (:use [clojure.contrib.repl-utils :only [show]])
+  (:use [clojure.pprint             :only [pprint]])
+  (:use [clojure.walk               :only [macroexpand-all]])
+  (:use clojure.contrib.math))
+
+; memory, not for using
+(defn prime-numbers-improved "Return the list of the n first prime numbers"
+  [n]
+  (if (zero? n)
+    []
+    (loop [candidate 3 current (dec n) primes [2]]
+      (if (zero? current)
+        primes
+        (if (not (some #(zero? (rem candidate %)) (take (floor (sqrt candidate)) primes)))
+          (recur (+ 2 candidate) (dec current) (conj primes candidate))
+          (recur (+ 2 candidate) current primes)
+          ))))
+  )
+
+(defn dec-prime-number "Compute the decomposition in prime numbers."
+  [n]
+  (cond
+   (<= n 1) []
+   (== 2 n) [2]
+   :else
+   (let [primes (prime-numbers-improved (ceil (sqrt n)))
+         divs (filter #(not= nil %) (map #(if (zero? (rem %1 %2)) %2) (repeat (count primes) n) primes))]
+     (loop [acc-all-divisors [] acc-div-primes divs num n]
+       (if (= 1 num)
+         acc-all-divisors
+         (if (= nil (first acc-div-primes))
+           [n]
+           (let [divisor-prime (first acc-div-primes)]
+             (if (zero? (rem num divisor-prime))
+               (recur (conj acc-all-divisors divisor-prime) acc-div-primes (/ num divisor-prime))
+               (recur acc-all-divisors (rest acc-div-primes) num)))))))))
+
+(fact
+  (dec-prime-number 0)            => []
+  (dec-prime-number 1)            => []
+  (dec-prime-number 2)            => [2]
+  (dec-prime-number 3)            => [3]
+  (dec-prime-number 4)            => [2 2]
+  (dec-prime-number 5)            => [5]
+  (dec-prime-number 10)           => [2 5]
+  (dec-prime-number 28)           => [2 2 7]
+  (dec-prime-number 53)           => [53]
+  (dec-prime-number 67)           => [67]
+  (dec-prime-number 76)           => [2 2 19]
+  (dec-prime-number 100)          => [2 2 5 5]
+  )
