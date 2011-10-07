0c342bcfc2dab6c059447492bdf1d1245ea60d3f
Antoine Romain Dumont <antoine.romain.dumont@gmail.com>
2011-10-07 12:38:27 +0200

    Rename file.

--- src/my_project_euler_lab/core-pb-11.clj
+++ src/my_project_euler_lab/core-pb-11.clj
@@ -1,4 +1,4 @@
- (ns my-project-euler-lab.core_pb_11
+ (ns my-project-euler-lab.core-pb-11
   (:use [clojure.test               :only [run-tests]])
   (:use [midje.sweet])
   (:use [clojure.contrib.repl-utils :only [show]])
