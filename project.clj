3cd7d39a69d9592bbab7e2d168d934f4b7e65fd5
Antoine Romain Dumont <antoine.romain.dumont@gmail.com>
2011-10-16 22:35:47 +0200

    Add the clj-time.core lib to help with manipulating date/time operations.

--- project.clj
+++ project.clj
@@ -1,5 +1,6 @@
 (defproject my-project-euler-lab "1.0.0-SNAPSHOT"
   :description "Solve euler problems"
-  :dependencies [[org.clojure/clojure "1.2.1"]]
+  :dependencies [[org.clojure/clojure "1.2.1"]
+                 [clj-time "0.3.1"]]
   :dev-dependencies [[org.clojure/clojure-contrib "1.2.0"]
                      [midje "1.2.0"]] )
