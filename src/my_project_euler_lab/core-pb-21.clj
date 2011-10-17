090cd78bf96e5914ec9d58d1338472a427489936
Antoine Romain Dumont <antoine.romain.dumont@gmail.com>
2011-10-17 07:55:34 +0200

    Top down TDD on the problem 21. Works with mocks at the moment. Need to test the reality.

--- src/my_project_euler_lab/core-pb-21.clj
+++ src/my_project_euler_lab/core-pb-21.clj
@@ -2,8 +2,7 @@
   (:use [clojure.test               :only [run-tests]])
   (:use [midje.sweet])
   (:use [clojure.contrib.repl-utils :only [show]])
-  (:use clj-time.core)
-  (:refer-clojure :exclude [extend])
+  (:use [my-project-euler-lab.primes :only [all-divisors]])
   )
 
 ;Let d(n) be defined as the sum of proper divisors of n (numbers less than n which divide evenly into n).
@@ -16,3 +15,69 @@
 
 ;Evaluate the sum of all the amicable numbers under 10000.
 
+(defn all-divisors-bl "Retrieve all the divisors except the last one (which is the numbers that starts all)"
+  [num]
+  (butlast (all-divisors num))
+  )
+
+(fact (all-divisors-bl 10) => [1 2 5])
+
+(defn amicable? "Heart of the problem - determine if a number is amicable or not"
+  [num]
+  (let [nb-divisors (all-divisors-bl num)
+        sum-divisors (reduce + nb-divisors)
+        nb-divisors-sum (reduce + (all-divisors-bl sum-divisors))]
+#_    (println "num" num "nb-divisors" nb-divisors "sum-divisors" sum-divisors "nb-divisors" nb-divisors)
+    (and (not= num sum-divisors) (= num nb-divisors-sum))
+    )
+  )
+
+(fact "Check the amicable? method returns correct number"
+  ; mock test
+  (amicable? 10) => true
+  (provided
+    (all-divisors-bl 10) => [1 3]
+    (all-divisors-bl 4) =>  [1 4 5])
+  (amicable? 10) => false
+  (amicable? 284) => true
+  (amicable? 220) => true
+  )
+
+(defn amicable-numbers-under "Determine the amicable numbers from an interval"
+  [borne]
+  (if (neg? borne) []
+      (filter #(not= nil %)
+              (map (fn [num]
+                     (if (amicable? num) num))
+                   (range 1 (inc borne)))))
+  )
+
+;.;. FAIL at (NO_SOURCE_FILE:1)
+;.;.     Expected: []
+;.;.       Actual: java.lang.RuntimeException: java.lang.RuntimeException: java.lang.RuntimeException: java.lang.ClassCastException: java.lang.Character cannot be cast to java.lang.Number
+;.;.               my_project_euler_lab.core_pb_21$eval7851$fn__7852.invoke(NO_SOURCE_FILE:1)
+;.;.               my_project_euler_lab.core_pb_21$eval7851.invoke(NO_SOURCE_FILE:1)
+;.;.               my_project_euler_lab.core_pb_21$eval7847.invoke(NO_SOURCE_FILE)
+(fact "Check the amicable numbers generating sequence."
+  ; mock test top-down tdd
+  (amicable-numbers-under 5) => [1 2 4 5]
+  (provided
+    (amicable? 1) => true
+    (amicable? 2) => true
+    (amicable? 3) => false
+    (amicable? 4) => true
+    (amicable? 5) => true)
+  ; limit use case
+  (amicable-numbers-under -1) => []
+;  (amicable-numbers-under 10) => []
+  )
+
+(defn sum-amicable-numbers "Compute the sum of amicable numbles given an interval"
+  [borne]
+  (reduce + (amicable-numbers-under 10000))
+  )
+
+(fact "Check that the sum of amicable numbers for an interval is ok."
+  (sum-amicable-numbers 10000) => 504
+  (provided (amicable-numbers-under 10000) => [220 284])
+  )
--- src/my_project_euler_lab/primes.clj
+++ src/my_project_euler_lab/primes.clj
@@ -97,10 +97,79 @@
                  (recur (inc curr))))
              (conj prevs n))))))
 
-;.;. Woohoo! -- @zspencer
 (fact
   (take 5 (lazy-primes))   => [2 3 5 7 11]
   (take 10 (lazy-primes))  => [2 3 5 7 11 13 17 19 23 29]
   (take 15 (lazy-primes))  => [2 3 5 7 11 13 17 19 23 29 31 37 41 43 47]
   (take 100 (lazy-primes)) => [ 2 3 5 7 11 13 17 19 23 29 31 37 41 43 47 53 59 61 67 71 73 79 83 89 97 101 103 107 109 113 127 131 137 139 149 151 157 163 167 173 179 181 191 193 197 199 211 223 227 229 233 239 241 251 257 263 269 271 277 281 283 293 307 311 313 317 331 337 347 349 353 359 367 373 379 383 389 397 401 409 419 421 431 433 439 443 449 457 461 463 467 479 487 491 499 503 509 521 523 541]
-)
+  )
+
+(defn all-divisors "Compute all divisors of a number"
+  [n]
+  (cond
+   (zero? n) "infinity"
+   (= 1 n) [n]
+   :else
+   (sort (distinct (map #(gcd n %) (range 2 (inc n)))))))
+
+;.;. For every disciplined effort, there is a multiple reward. -- Rohn
+(fact
+  (all-divisors 0) => "infinity"
+  (all-divisors 1) => [1]
+  (all-divisors 10) => [1 2 5 10]
+  (all-divisors 15) => [1 3 5 15]
+  (all-divisors 21) => [1 3 7 21]
+  (all-divisors 28) => [1 2 4 7 14 28]
+  (all-divisors 120) => [1 2 3 4 5 6 8 10 12 15 20 24 30 40 60 120]
+  )
+
+(defn count-divisors-with-dec-prime "Compute the number of divisors with the help of the decomposition in prime numbers."
+  [primes n]
+  (cond
+   (<= n 1) 0;principles -> infinity
+   :else
+   (loop [nb-divisors 1, cnt-exp 0, acc-div-primes primes, num n]
+     (if (= 1 num)
+       (* nb-divisors (inc cnt-exp))
+       (let [divisor-prime (first acc-div-primes)]
+         (if (zero? (rem num divisor-prime))
+           (recur nb-divisors (inc cnt-exp) acc-div-primes (/ num divisor-prime))
+           (recur (* nb-divisors (inc cnt-exp)) 0 (rest acc-div-primes) num)))))))
+
+(def litte-primes [2 3 5 7 11 13 17 19 23 29 31 37 41 43 47 53 59 61 67 71 73 79 83 89 97 101 103 107])
+
+(fact
+ (count-divisors-with-dec-prime [] 1) => 0
+ (count-divisors-with-dec-prime [] 0) => 0
+ (count-divisors-with-dec-prime litte-primes 10) => 4
+ (count-divisors-with-dec-prime litte-primes 15) => 4
+ (count-divisors-with-dec-prime litte-primes 21) => 4
+ (count-divisors-with-dec-prime litte-primes 28) => 6
+ (count-divisors-with-dec-prime (take 20 (lazy-primes)) 120) => 16
+ )
+
+(defn all-divisors-with-dec-prime "Compute the divisors with the help of the decomposition in prime numbers."
+  [primes n]
+  (cond
+   (<= n 1) [];principles -> infinity
+   :else
+   (loop [acc-divisors [], acc-div-primes primes, num n]
+     (if (= 1 num)
+       acc-divisors
+       (let [divisor-prime (first acc-div-primes)]
+         (if (zero? (rem num divisor-prime))
+           (recur (conj acc-divisors divisor-prime) acc-div-primes (/ num divisor-prime))
+           (recur acc-divisors (rest acc-div-primes) num)))))))
+
+;.;. FAIL at (NO_SOURCE_FILE:1)
+;.;.     Expected: [2 4 7 14 28]
+;.;.       Actual: [2 2 7]
+(fact
+ (all-divisors-with-dec-prime [] 1) => []
+ (all-divisors-with-dec-prime [] 0) => []
+ (all-divisors-with-dec-prime litte-primes 10) => [2 5]
+ (all-divisors-with-dec-prime litte-primes 15) => [3 5]
+ (all-divisors-with-dec-prime litte-primes 21) => [3 7]
+ (all-divisors-with-dec-prime litte-primes 28) => [2 4 7 14 28]
+; (all-divisors-with-dec-prime (take 20 (lazy-primes)) 120) => [1 2 3 4 5 6 8 10 12 15 20 24 30 40 60 120]
+ )
