ae1bc9a005f192be7f024075806d849804b14336
Antoine Romain Dumont <antoine.romain.dumont@gmail.com>
2011-10-22 18:24:39 +0200

    Improve the simple computation of all the divisors of a numbers. Add the all-divisors-bi method to compute all the divisors of a numbers but itself.

--- src/my_project_euler_lab/primes.clj
+++ src/my_project_euler_lab/primes.clj
@@ -4,7 +4,9 @@
   (:use [clojure.contrib.repl-utils :only [show]])
   (:use [clojure.pprint             :only [pprint]])
   (:use [clojure.walk               :only [macroexpand-all]])
-  (:use clojure.contrib.math))
+  (:use [ clojure.set :only [difference]])
+  (:use clojure.contrib.math)
+  )
 
 ; memory, not for using
 (defn prime-numbers-improved "Return the list of the n first prime numbers"
@@ -104,41 +106,56 @@
   (take 100 (lazy-primes)) => [ 2 3 5 7 11 13 17 19 23 29 31 37 41 43 47 53 59 61 67 71 73 79 83 89 97 101 103 107 109 113 127 131 137 139 149 151 157 163 167 173 179 181 191 193 197 199 211 223 227 229 233 239 241 251 257 263 269 271 277 281 283 293 307 311 313 317 331 337 347 349 353 359 367 373 379 383 389 397 401 409 419 421 431 433 439 443 449 457 461 463 467 479 487 491 499 503 509 521 523 541]
   )
 
-(defn all-divisors "Compute all divisors of a number"
+(defn all-divisors "Compute all divisors of a number (improved version)"
   [n]
   (cond
    (zero? n) "infinity"
    (= 1 n) [n]
    (= 2 n) [1 2]
    :else
-   (sort (distinct (map #(gcd n %) (range 2 (inc n)))))))
+   (loop [acc #{} num (ceil (sqrt (inc n)))]
+     (if (zero? num)
+       acc
+       (if (zero? (rem n num))
+         (recur (conj acc num (/ n num)) (dec num)) 
+         (recur acc (dec num)))))))
 
 (fact
   (all-divisors 0) => "infinity"
   (all-divisors 1) => [1]
-  (all-divisors 2) => [1 2]
-  (all-divisors 10) => [1 2 5 10]
-  (all-divisors 15) => [1 3 5 15]
-  (all-divisors 21) => [1 3 7 21]
-  (all-divisors 28) => [1 2 4 7 14 28]
-  (all-divisors 120) => [1 2 3 4 5 6 8 10 12 15 20 24 30 40 60 120]
+  (all-divisors 2) => (contains [1 2] :in-any-order)
+  (all-divisors 10) => (contains [1 2 5 10] :in-any-order)
+  (all-divisors 15) => (contains [1 3 5 15] :in-any-order)
+  (all-divisors 21) => (contains [1 3 7 21] :in-any-order)
+  (all-divisors 28) => (contains [1 2 4 7 14 28] :in-any-order)
+  (all-divisors 120) => (contains [1 2 3 4 5 6 8 10 12 15 20 24 30 40 60 120] :in-any-order)
   )
 
-(defn all-divisors-bl "Compute all divisors of a number"
+(defn all-divisors-bi "Compute all divisors of a number but itself."
   [n]
-  (butlast (all-divisors n))
-)
+  (cond
+   (zero? n) "infinity"
+   (= 1 n) nil
+   (= 2 n) [1]
+   :else
+   (loop [acc #{} num (ceil (sqrt (inc n)))]
+     (if (= 1 num)
+       (conj acc 1)
+       (if (zero? (rem n num))
+         (recur (conj acc num (/ n num)) (dec num)) 
+         (recur acc (dec num)))))))
 
-;.;. The sum of wisdom is that time is never lost that is devoted to
-;.;. work. -- Emerson
+;.;. Before the reward there must be labor. You plant before you
+;.;. harvest. You sow in tears before you reap joy. -- Ransom
 (fact
-  (all-divisors-bl 1) => nil
-  (all-divisors-bl 2) => [1]
-  (all-divisors-bl 10) => [1 2 5]
-  (all-divisors-bl 15) => [1 3 5]
-  (all-divisors-bl 21) => [1 3 7]
-  (all-divisors-bl 28) => [1 2 4 7 14]
-  (all-divisors-bl 120) => [1 2 3 4 5 6 8 10 12 15 20 24 30 40 60]
+  (all-divisors-bi 0) => "infinity"
+  (all-divisors-bi 1) => nil
+  (all-divisors-bi 2) => (contains [1] :in-any-order)
+  (all-divisors-bi 10) => (contains [1 2 5] :in-any-order)
+  (all-divisors-bi 15) => (contains [1 3 5] :in-any-order)
+  (all-divisors-bi 21) => (contains [1 3 7] :in-any-order)
+  (all-divisors-bi 28) => (contains [1 2 4 7 14] :in-any-order)
+  (all-divisors-bi 120) => (contains [1 2 3 4 5 6 8 10 12 15 20 24 30 40 60] :in-any-order)
   )
 
 (defn count-divisors-with-dec-prime "Compute the number of divisors with the help of the decomposition in prime numbers."
@@ -154,15 +171,15 @@
            (recur nb-divisors (inc cnt-exp) acc-div-primes (/ num divisor-prime))
            (recur (* nb-divisors (inc cnt-exp)) 0 (rest acc-div-primes) num)))))))
 
-(def litte-primes [2 3 5 7 11 13 17 19 23 29 31 37 41 43 47 53 59 61 67 71 73 79 83 89 97 101 103 107])
+(def little-primes [2 3 5 7 11 13 17 19 23 29 31 37 41 43 47 53 59 61 67 71 73 79 83 89 97 101 103 107])
 
 (fact
  (count-divisors-with-dec-prime [] 1) => 0
  (count-divisors-with-dec-prime [] 0) => 0
- (count-divisors-with-dec-prime litte-primes 10) => 4
- (count-divisors-with-dec-prime litte-primes 15) => 4
- (count-divisors-with-dec-prime litte-primes 21) => 4
- (count-divisors-with-dec-prime litte-primes 28) => 6
+ (count-divisors-with-dec-prime little-primes 10) => 4
+ (count-divisors-with-dec-prime little-primes 15) => 4
+ (count-divisors-with-dec-prime little-primes 21) => 4
+ (count-divisors-with-dec-prime little-primes 28) => 6
  (count-divisors-with-dec-prime (take 20 (lazy-primes)) 120) => 16
  )
 
@@ -174,17 +191,19 @@
    (loop [acc-divisors [], acc-div-primes primes, num n]
      (if (= 1 num)
        acc-divisors
-       (let [divisor-prime (first acc-div-primes)]
+       (let [divisor-prime (first acc-div-primes)
+             divisor-n (/ num divisor-prime)
+             ]
          (if (zero? (rem num divisor-prime))
-           (recur (conj acc-divisors divisor-prime) acc-div-primes (/ num divisor-prime))
+           (recur (conj acc-divisors divisor-prime divisor-n) acc-div-primes divisor-n)
            (recur acc-divisors (rest acc-div-primes) num)))))))
 
-(fact
+(fact "Finish the implem all the divisors with the decomposition in prime number"
  (all-divisors-with-dec-prime [] 1) => []
  (all-divisors-with-dec-prime [] 0) => []
- (all-divisors-with-dec-prime litte-primes 10) => [2 5]
- (all-divisors-with-dec-prime litte-primes 15) => [3 5]
- (all-divisors-with-dec-prime litte-primes 21) => [3 7]
- (all-divisors-with-dec-prime litte-primes 28) => [2 4 7 14 28]
+ (all-divisors-with-dec-prime little-primes 10) => [2 5]
+ (all-divisors-with-dec-prime little-primes 15) => [3 5]
+ (all-divisors-with-dec-prime little-primes 21) => [3 7]
+ (all-divisors-with-dec-prime little-primes 28) => [2 4 7 14 28]
 ; (all-divisors-with-dec-prime (take 20 (lazy-primes)) 120) => [1 2 3 4 5 6 8 10 12 15 20 24 30 40 60 120]
  )
