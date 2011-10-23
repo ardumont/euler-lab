bf3a86c39300bb3ca2cee5dbb08f3a3944fe1162
Antoine Romain Dumont <antoine.romain.dumont@gmail.com>
2011-10-23 11:35:13 +0200

    Remove the code that is not needed.

--- src/my_project_euler_lab/core-pb-23.clj
+++ src/my_project_euler_lab/core-pb-23.clj
@@ -4,7 +4,7 @@
   (:use [clojure.contrib.repl-utils :only [show]])
   (:use [clojure.set :only [difference]] )
   (:use clojure.contrib.math)
-  (:use [my-project-euler-lab.primes :only [all-divisors-bl]] )
+  (:use [my-project-euler-lab.primes :only [all-divisors-bi]] )
   )
 
 ;A perfect number is a number for which the sum of its proper divisors
@@ -29,173 +29,76 @@
 (unfinished )
 
 (defn all-dec-in-sums "Find all the sums possibles that gives n with 2 integers"
-  [n]
+  [integers n]
   (let [half-n (ceil (/ n 2))
-        integers-s (range 1 (inc half-n))]
+        integers-s (take half-n integers)]
     (reduce conj #{}
             (map
              #(vec [% (- n %)])
              integers-s))))
 
 (fact
-  (all-dec-in-sums 10) => #{[1 9] [2 8] [3 7] [4 6] [5 5]}
-  (all-dec-in-sums 11) => #{[6 5] [5 6] [4 7] [3 8] [2 9] [1 10]}
-  )
-
-(defn sum "Compute the sum of the vector of integers"
-  [v]
-  (reduce + v))
-
-(fact
-  (sum [1 2 4]) => 7)
-
-(defn what-is? "Compute the divisors of a numbers and generate a matrix of result to dispatch its qualities (perfect, abundant, deficient)"
-  [n]
-  (let [sum-all-divisors (sum (all-divisors-bl n))]
-    (cond
-     (= n sum-all-divisors) {:perfect true :abundant false :deficient false}
-     (< n sum-all-divisors) {:perfect false :abundant true :deficient false}
-     (> n sum-all-divisors) {:perfect false :abundant false :deficient true}
-     )
-    )
-  )
-
-(fact
-  (what-is? 10) => {:perfect true :abundant false :deficient false}
-  (provided
-    (all-divisors-bl 10) => [1 2 4]
-    (sum [1 2 4]) => 10)
-  ; it test
-  (what-is? 10) => {:perfect false :abundant false :deficient true}
-  (what-is? 28) => {:perfect true :abundant false :deficient false}
-  (what-is? 120) => {:perfect false :abundant true :deficient false}
-  )
-
-(defn deficient? "Test if a mumber is deficient (sum of all its divisors inferior to itself)"
-  [n]
-  (let [result (what-is? n)]
-    (= true (result :deficient) )
-    )
-  )
-
-(fact
-  (deficient? 10) => true
-  (provided
-    (what-is? 10) => {:deficient true})
-  (deficient? 20) => false
-  (provided
-    (what-is? 20) => {:deficient false})
-  ; it test
-  (deficient? 10) => true
-  (deficient? 28) => false
-  )
-
-(defn perfect? "Test if a mumber is a perfect number"
-  [n]
-  (let [result (what-is? n)]
-    (= true (result :perfect) )
-    )
-  )
-
-(fact
-  (perfect? 10) => true
-  (provided
-    (what-is? 10) => {:perfect true})
-  (perfect? 20) => false
-  (provided
-    (what-is? 20) => {:perfect false})
-  ; it test
-  (perfect? 10) => false
-  (perfect? 28) => true
+  (all-dec-in-sums (iterate inc 1) 10) => #{[1 9] [2 8] [3 7] [4 6] [5 5]}
+  (all-dec-in-sums (iterate inc 1) 11) => #{[6 5] [5 6] [4 7] [3 8] [2 9] [1 10]}
   )
 
 (defn abundant? "Test if a number is abundant (sums of its divisors is superior to itself)"
   [n]
-  (let [result (what-is? n)]
-    (= true (result :abundant) )
-    )
-  )
+  (let [sum-all-divisors (reduce + (all-divisors-bi n))]
+    (< n sum-all-divisors)))
 
 (fact
-  (abundant? 10) => true
-  (provided
-    (what-is? 10) => {:abundant true})
-  (abundant? 20) => false
-  (provided
-    (what-is? 20) => {:abundant false})
-  ; it test
   (abundant? 10) => false
   (abundant? 120) => true
   )
 
 (defn sum-two-abundant? "Can the number be a sum of 2 abundants numbers?"
-  [n]
-  (let [all-sums (all-dec-in-sums n)]
-    (some (fn [e] (= true e))
-          (map #(and (abundant? (first %)) (abundant? (second %))) all-sums))))
+  [set-integers-23 n]
+  (let [all-sums (all-dec-in-sums set-integers-23 n)]
+    (some #(and (abundant? (first %)) (abundant? (second %))) all-sums)))
 
 (fact
-  (sum-two-abundant? 10) => falsey
+  (sum-two-abundant? :set-integers-23 10) => falsey
   (provided
-    (all-dec-in-sums 10) => [[5 5] [9 1]]
+    (all-dec-in-sums :set-integers-23 10) => #{[5 5] [9 1]}
     (abundant? 5) => false
     (abundant? 9) => false)
   )
 
 (fact
-  (sum-two-abundant? 10) => true
+  (sum-two-abundant? :set-integers-23 10) => true
   (provided
-    (all-dec-in-sums 10) => [[5 5] [9 1]]
+    (all-dec-in-sums :set-integers-23 10) => #{[5 9] [8 2]}
     (abundant? 5) => true
-    (abundant? 9) => false)
+    (abundant? 9) => true)
   )
 
 ; it test
 (fact
-    (sum-two-abundant? 240) => true)
-
-(defn integers-numbers "Compute the set of integers from 1 to n"
-  [n]
-    {:pre [(pos? n)]}
-    (set (range 1 (inc n))))
-
-(fact
-  (integers-numbers 5) => #{1 2 3 4 5}
-  (integers-numbers -1) => (throws AssertionError)
+  (sum-two-abundant? (iterate inc 0) 240) => true
+  (sum-two-abundant? (iterate inc 0) 28123) => true
   )
 
-(defn abuntant-numbers
-  [n]
-  {:pre [(pos? n)]}
-  (let [ints (integers-numbers n)]
-    (set (filter #(not= nil %) (map (fn [num] (if (sum-two-abundant? num) num)) ints)))))
+(defn abundant-numbers
+  [set-integers-23 n]
+  (set (map (fn [num] (if (sum-two-abundant? set-integers-23 num) num))
+             (take n set-integers-23))))
 
+;.;. I do believe in praising that which deserves to be praised. -- Dean
+;.;. Smith
 (fact
-  (abuntant-numbers 50) => #{12 24}
-  (provided
-    (integers-numbers 50) => #{12 24 48}
-    (sum-two-abundant? 12) => true
-    (sum-two-abundant? 24) => true
-    (sum-two-abundant? 48) => false
-    )
-  (abuntant-numbers -1) => (throws AssertionError)
-  )
-
-; it test
-;.;. The reward of a thing well done is to have done it. -- Emerson
-(fact (abuntant-numbers 50) => #{32 36 38 40 42 44 48 50 24 30})
+  (abundant-numbers (iterate inc 1) 50) => #{nil 32 36 38 40 42 44 48 50 24 30}
+  (abundant-numbers (iterate inc 1) 100) => #{nil 32 64 96 66 98 36 68 100 38 70 40 72 42 74 44 76 78 48 80 50 82 52 84 54 86 24 56 88 58 90 60 92 30 62 94})
 
 (defn sum-all-positive-integer-euler-23 "Compute the sum of all positive integer which cannot be written as the sum of two abundant numbers in the interval 12 - n"
-  [n]
-  (reduce + (clojure.set/difference (integers-numbers n) (abuntant-numbers n)))
-  )
+  [set-integers-23 n]
+  (let [set-integers-23-s (conj (take n set-integers-23) nil)]
+    (reduce +
+            (difference
+             set-integers-23-s
+             (abundant-numbers set-integers-23 n)))))
 
-(fact "sum of all positive integers which cannot be written as the sum of two abundant numbers."
-  (sum-all-positive-integer-euler-23 28123) => 3
-  (provided
-    (integers-numbers 28123) => #{1 2 3}
-    (abuntant-numbers 28123) => #{3}
-    )
-  (sum-all-positive-integer-euler-23 -1) => (throws AssertionError)
-  )
+(future-fact "Test the reality"
+  (sum-all-positive-integer-euler-23 (iterate inc 1) 28123) => #{})
 
+(println "--------- END OF PB 23 ----------" (java.util.Date.))
