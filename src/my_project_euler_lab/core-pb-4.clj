633c265aab8d00d50a3c2bd7be0f9cc6c4a38912
Antoine R. Dumont <antoine.romain.dumont@gmail.com>
2011-12-03 20:40:29 +0100

    Improve readability.

--- src/my_project_euler_lab/core-pb-4.clj
+++ src/my_project_euler_lab/core-pb-4.clj
@@ -16,7 +16,7 @@
 
 (defn pal? "Is the sequence given a palindrome?"
   [v]
-  (= v (reverse v)))
+  (= (seq v) (reverse v)))
 
 (fact
   (pal? [1 2 3]) => falsey
@@ -31,24 +31,24 @@
 
 (defn gen-all-mult "Create the sequence of product from all the elements in the sequence 2x2. The product are filtered to not be above the limit."
   [seq0 limit]
-  (distinct
-   (for [x seq0 y seq0] (let [pdt (* x y)] (if (<= pdt limit) pdt)))))
+  (distinct 
+   (for [x seq0 y seq0 :when (<= (* x y) limit)]  (* x y))))
 
+;.;. The biggest reward for a thing well done is to have done it. --
+;.;. Voltaire
 (fact
   (gen-all-mult (range 1 11) 100)
-  => (contains [10 20 30 40 50 60 70 80 90 100 9 18 27 36 45 54 63 72 81 8 16 24 32 48 56 64 7 14 21 28 35 42 49 6 12 5 15 25 4 3 2 1] :in-any-order)
+  => (contains [10 20 30 40 50 60 70 80 90 100 9 18 27 36 45 54 63 72 81 8 16 24 32 48 56 64 7 14 21 28 35 42 49 6 12 5 15 25 4 3 2 1] :in-any-order :gaps-ok)
     (gen-all-mult (range 1 11) 10)
-  => (contains [10 9 8 7 6 5 4 3 2 1] :in-any-order))
+  => (contains [10 9 8 7 6 5 4 3 2 1] :in-any-order :gaps-ok))
 
 (defn max-pal-6 "Find the largest palindrome"
   []
   (reduce
    max
    (filter
-    #(and (not= nil %) (pal? (num-digits-into-vec %)))
+    #(and (not= nil %) (pal? (str %)))
     (gen-all-mult (range 100 1000) 1000000))))
 
-;.;. One small test for a codebase, one giant leap for quality kind! --
-;.;. @zspencer
 (fact (max-pal-6) => 906609)
 
