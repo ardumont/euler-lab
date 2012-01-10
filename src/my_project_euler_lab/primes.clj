c2e6f0a050e4788e34e03705fc0c7ce78fb01f8b
Antoine R. Dumont <antoine.romain.dumont@gmail.com>
2012-01-10 22:26:40 +0100

    Syntax.

--- src/my_project_euler_lab/primes.clj
+++ src/my_project_euler_lab/primes.clj
@@ -5,8 +5,7 @@
   (:use [clojure.pprint             :only [pprint]])
   (:use [clojure.walk               :only [macroexpand-all]])
   (:use [ clojure.set :only [difference]])
-  (:use clojure.contrib.math)
-  )
+  (:use clojure.contrib.math))
 
 ; memory, not for using
 (defn prime-numbers-improved "Return the list of the n first prime numbers"
@@ -18,14 +17,12 @@
         primes
         (if (not (some #(zero? (rem candidate %)) (take (floor (sqrt candidate)) primes)))
           (recur (+ 2 candidate) (dec current) (conj primes candidate))
-          (recur (+ 2 candidate) current primes)
-          )))))
+          (recur (+ 2 candidate) current primes))))))
 
 (fact
   (prime-numbers-improved 5) => [2 3 5 7 11]
   (prime-numbers-improved 10) => [2 3 5 7 11 13 17 19 23 29]
-  (prime-numbers-improved 15) => [2 3 5 7 11 13 17 19 23 29 31 37 41 43 47]
-  )
+  (prime-numbers-improved 15) => [2 3 5 7 11 13 17 19 23 29 31 37 41 43 47])
 
 (defn dec-prime-numbers-with-primes "Compute the decomposition in prime numbers."
   [primes n]
@@ -73,38 +70,35 @@
   (dec-prime-numbers 53)           => [53]
   (dec-prime-numbers 67)           => [67]
   (dec-prime-numbers 76)           => [2 2 19]
-  (dec-prime-numbers 100)          => [2 2 5 5]
-  )
+  (dec-prime-numbers 100)          => [2 2 5 5])
 
 (defn prime? "Is the number a prime?"
   [n primes-seq]
-  (not (some #(zero? (rem n %)) (take (floor (sqrt n)) primes-seq)))
-  )
+  (not (some #(zero? (rem n %)) (take (floor (sqrt n)) primes-seq))))
 
 (fact
   (prime? 11 [2 3 5]) => true
   (prime? 12 [2 3 5]) => false
-  (prime? 67 [2 3 5 7 11 13 17]) => true
-  )
+  (prime? 67 [2 3 5 7 11 13 17]) => true)
 
 (defn lazy-primes "A lazy seq of primes, using the 'n is prime if not divisible by an int > sqrt n' optimization"
   ([]  (lazy-primes 2 []))
   ([n prevs]
      (lazy-seq
-      (cons n
-            (lazy-primes
-             (loop [curr (inc n)]
-               (if (prime? curr prevs)
-                 curr
-                 (recur (inc curr))))
-             (conj prevs n))))))
+      (cons
+       n
+       (lazy-primes
+        (loop [curr (inc n)]
+          (if (prime? curr prevs)
+            curr
+            (recur (inc curr))))
+        (conj prevs n))))))
 
 (fact
   (take 5 (lazy-primes))   => [2 3 5 7 11]
   (take 10 (lazy-primes))  => [2 3 5 7 11 13 17 19 23 29]
   (take 15 (lazy-primes))  => [2 3 5 7 11 13 17 19 23 29 31 37 41 43 47]
-  (take 100 (lazy-primes)) => [ 2 3 5 7 11 13 17 19 23 29 31 37 41 43 47 53 59 61 67 71 73 79 83 89 97 101 103 107 109 113 127 131 137 139 149 151 157 163 167 173 179 181 191 193 197 199 211 223 227 229 233 239 241 251 257 263 269 271 277 281 283 293 307 311 313 317 331 337 347 349 353 359 367 373 379 383 389 397 401 409 419 421 431 433 439 443 449 457 461 463 467 479 487 491 499 503 509 521 523 541]
-  )
+  (take 100 (lazy-primes)) => [ 2 3 5 7 11 13 17 19 23 29 31 37 41 43 47 53 59 61 67 71 73 79 83 89 97 101 103 107 109 113 127 131 137 139 149 151 157 163 167 173 179 181 191 193 197 199 211 223 227 229 233 239 241 251 257 263 269 271 277 281 283 293 307 311 313 317 331 337 347 349 353 359 367 373 379 383 389 397 401 409 419 421 431 433 439 443 449 457 461 463 467 479 487 491 499 503 509 521 523 541])
 
 (defn all-divisors "Compute all divisors of a number (improved version)"
   [n]
@@ -128,8 +122,7 @@
   (all-divisors 15) => (contains [1 3 5 15] :in-any-order)
   (all-divisors 21) => (contains [1 3 7 21] :in-any-order)
   (all-divisors 28) => (contains [1 2 4 7 14 28] :in-any-order)
-  (all-divisors 120) => (contains [1 2 3 4 5 6 8 10 12 15 20 24 30 40 60 120] :in-any-order)
-  )
+  (all-divisors 120) => (contains [1 2 3 4 5 6 8 10 12 15 20 24 30 40 60 120] :in-any-order))
 
 (defn all-divisors-bi "Compute all divisors of a number but itself."
   [n]
@@ -154,8 +147,7 @@
   (all-divisors-bi 15) => (contains [1 3 5] :in-any-order)
   (all-divisors-bi 21) => (contains [1 3 7] :in-any-order)
   (all-divisors-bi 28) => (contains [1 2 4 7 14] :in-any-order)
-  (all-divisors-bi 120) => (contains [1 2 3 4 5 6 8 10 12 15 20 24 30 40 60] :in-any-order)
-  )
+  (all-divisors-bi 120) => (contains [1 2 3 4 5 6 8 10 12 15 20 24 30 40 60] :in-any-order))
 
 (defn count-divisors-with-dec-prime "Compute the number of divisors with the help of the decomposition in prime numbers."
   [primes n]
@@ -179,8 +171,7 @@
  (count-divisors-with-dec-prime little-primes 15) => 4
  (count-divisors-with-dec-prime little-primes 21) => 4
  (count-divisors-with-dec-prime little-primes 28) => 6
- (count-divisors-with-dec-prime (take 20 (lazy-primes)) 120) => 16
- )
+ (count-divisors-with-dec-prime (take 20 (lazy-primes)) 120) => 16)
 
 (defn all-divisors-with-dec-prime "Compute the divisors with the help of the decomposition in prime numbers."
   [primes n]
@@ -204,8 +195,9 @@
  (all-divisors-with-dec-prime little-primes 10) => #{1 2 5}
  (all-divisors-with-dec-prime little-primes 15) => #{1 3 5}
  (all-divisors-with-dec-prime little-primes 21) => #{1 3 7}
- (all-divisors-with-dec-prime little-primes 28) => #{1 2 4 7 14}
-; (all-divisors-with-dec-prime (take 20 (lazy-primes)) 120) => #{1 2 3 4 5 6 8 10 12 15 20 24 30 40 60}
- )
+ (all-divisors-with-dec-prime little-primes 28) => #{1 2 4 7 14})
+
+(comment
+  (fact (all-divisors-with-dec-prime (take 20 (lazy-primes)) 120) => #{1 2 3 4 5 6 8 10 12 15 20 24 30 40 60}))
 
 (println "--------- END OF PRIMES ----------" (java.util.Date.))
