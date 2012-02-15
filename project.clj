cb5de01128f4907be23623c238b5e49bc1a9465e
Antoine R. Dumont <antoine.romain.dumont@gmail.com>
2012-02-15 21:16:37 +0100

    Upgrade libs.

--- project.clj
+++ project.clj
@@ -1,6 +1,8 @@
 (defproject my-project-euler-lab "1.0.0-SNAPSHOT"
   :description "Solve euler problems"
-  :dependencies [[org.clojure/clojure "1.2.1"]
-                 [clj-time "0.3.1"]]
-  :dev-dependencies [[org.clojure/clojure-contrib "1.2.0"]
-                     [midje "1.2.0"]] )
+  :dependencies [[org.clojure/clojure "1.3.0"]
+                 [clj-time "0.3.1"]
+                 [clj-stacktrace      "0.2.4"]]
+  :dev-dependencies [[midje "1.3.1"]
+                     [com.intelie/lazytest "1.0.0-SNAPSHOT" :exclusions [swank-clojure]]
+                     [lein-marginalia "0.7.0-SNAPSHOT"]] )
