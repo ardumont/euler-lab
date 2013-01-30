fccad214849a611f3d319d268301f573dbb4cb1b
Antoine R. Dumont <antoine.romain.dumont@gmail.com>
2013-01-30 20:41:40 +0100

    Update deps

--- project.clj
+++ project.clj
@@ -1,9 +1,8 @@
 (defproject my-project-euler-lab "1.0.0-SNAPSHOT"
   :description "Solve euler problems"
-  :dependencies [[org.clojure/clojure "1.3.0"]
-                 [clojure-contrib "1.2.0"]
-                 [clj-time "0.3.1"]
-                 [clj-stacktrace      "0.2.4"]]
-  :dev-dependencies [[midje "1.3.1"]
-                     [com.intelie/lazytest "1.0.0-SNAPSHOT" :exclusions [swank-clojure]]
-                     [lein-marginalia "0.7.0-SNAPSHOT"]] )
+  :dependencies [[org.clojure/clojure      "1.4.0"]
+                 [clj-time                 "0.3.1"]
+                 [clj-stacktrace           "0.2.4"]
+                 [clj-http                 "0.6.3"]
+                 [midje                    "1.4.0"]]
+  :dev-dependencies [[com.intelie/lazytest "1.2.3"]])
