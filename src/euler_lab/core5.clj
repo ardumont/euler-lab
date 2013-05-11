4e3b553a8202d9cd2dd37d6177f46e3c7706e916
Antoine R. Dumont <antoine.romain.dumont@gmail.com>
2013-05-11 19:07:11 +0200

    Formatting

--- src/euler_lab/core5.clj
+++ src/euler_lab/core5.clj
@@ -1,15 +1,9 @@
 (ns euler-lab.core5
-  (:use [clojure.test               :only [run-tests]])
-  (:use [midje.sweet])
-  (:use [clojure.pprint             :only [pprint]]))
+  "Problem 5 - http://projecteuler.net/problem=5
+2520 is the smallest number that can be divided by each of the numbers from 1 to 10 without any remainder.
 
-                                        ; problem 5
-
-;; 2520 is the smallest number that can be divided by each of the numbers from 1 to 10 without any remainder.
-;; (fact (reduce + (map #(rem 2520 %) (range 2 11))) => 0)
-
-;; What is the smallest positive number that is evenly divisible by all
-;; of the numbers from 1 to 20?
+What is the smallest positive number that is evenly divisible by all of the numbers from 1 to 20?"
+  (:require [midje.sweet :as m]))
 
 ;; brute force
 
@@ -17,7 +11,7 @@
   [num borne]
   (not (some #(not= 0 %) (map #(rem num %) (range 2 (inc borne))))))
 
-(fact
+(m/fact
   (euler-5-ok? 2520 10) => true
   (euler-5-ok? 2521 10) => false)
 
@@ -25,6 +19,5 @@
   [incr start rg]
   (first (take 1 (drop-while #(not (euler-5-ok? % rg)) (iterate #(+ incr %) start)))))
 
-;.;. If this isn't nice, I don't know what is. -- Vonnegut
-(fact (find-smallest-ok-euler-5 2 10 10) => 2520)
-;(fact (find-smallest-ok-euler-5 2 20 20) => 232792560)
+(m/fact (find-smallest-ok-euler-5 2 10 10) => 2520)
+;(m/fact (find-smallest-ok-euler-5 2 20 20) => 232792560)
