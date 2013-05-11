b85ba964f6b3acc2d5c49fb6c740ee2a7f2553c4
Antoine R. Dumont <antoine.romain.dumont@gmail.com>
2013-05-11 18:50:26 +0200

    Upgrading to latest clojure and midje versions.

--- project.clj
+++ project.clj
@@ -1,9 +1,8 @@
 (defproject euler-lab "1.0.0-SNAPSHOT"
   :description "Solving euler problems."
-  :dependencies [[org.clojure/clojure            "1.4.0"]
+  :dependencies [[org.clojure/clojure            "1.5.1"]
                  [clj-time                       "0.3.1"]
                  [clj-stacktrace                 "0.2.4"]
                  [clj-http                       "0.6.3"]
-                 [midje                          "1.4.0"]
-                 [org.clojure/math.numeric-tower "0.0.2"]]
-  :dev-dependencies [[com.intelie/lazytest       "1.2.3"]])
+                 [midje                          "1.5.1"]
+                 [org.clojure/math.numeric-tower "0.0.2"]])
